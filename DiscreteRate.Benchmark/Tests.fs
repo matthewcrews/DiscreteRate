847bc7248c135b8da432492f8057532bc81dd850
Matthew <matthewcrews@gmail.com>
2021-04-29 12:02:21 -0700

    test setup

--- DiscreteRate.Benchmark/Tests.fs
+++ DiscreteRate.Benchmark/Tests.fs
@@ -4,32 +4,41 @@
 open DiscreteRate
 open BenchmarkDotNet.Attributes
 
+let operationCount = 10
 
-let source = Tank.create "Source" infinity
-let op1 = Operation.create "Operation1" 1.0 10.0
-let op2 = Operation.create "Operation2" 1.0 5.0
-let merge = Merge.create "Merge1"
-let split = Split.create "Split1"
-let tank1 = Tank.create "Tank1" 10.0
-let sink = Tank.create "Sink" infinity
+let operations =
+    [1 .. operationCount - 1]
+    |> List.map (fun idx -> idx, Operation.create $"Operation{idx}" 1.0 10.0)
+
+let tanks =
+    [0 .. operationCount - 1]
+    |> List.map (fun idx -> idx, Tank.create $"Tank{idx}" 10.0)
+    |> Map
+
+let sourceTank = tanks.[0]
+
+let arcs =
+    operations
+    |> List.collect (fun (idx, operation) -> 
+        let inArc = arc.connect (tanks.[idx - 1], operation)
+        let outArc = arc.connect (operation, tanks.[idx])
+        [inArc; outArc]
+    )
 
 let network =
-    Network [
-        arc.connect (source, split)
-        arc.connect (split, op1, 2.0)
-        arc.connect (split, op2, 1.0)
-        arc.connect (op1, sink)
-        arc.connect (op2, sink)
-    ]
+    Network arcs
 
-let initialLevels = 
-    [
-        source, TankLevel.Infinite
-        sink, TankLevel.Finite 0.0
-        //tank1, TankLevel.Finite 10.0
-    ] |> Map
+let tankLevels =
+    tanks
+    |> Map.toSeq
+    |> Seq.map snd
+    |> Seq.map (fun tank -> tank, TankLevel.Finite 0.0)
+    |> Map
+    |> fun x -> x.Add (sourceTank, TankLevel.Infinite)
 
-let initialState = { TankLevels = initialLevels }
+
+let initialState = { TankLevels = tankLevels }
+
 
 let nonMemoizeSettings = {
     MaxSolveTime_ms = 10_000L
@@ -45,17 +54,26 @@ let memoizedSettings = {
 
 let memoizedSolver = Solver memoizedSettings
 
+let maxIterations = 1_000
 
 type Benchmarks () =
 
     [<Benchmark>]
     member this.NonMemoized () =
-        let sln = nonMemoizedSolver.Solve network initialState
-        if sln.FillRates.[source].Value > 1_000_000.0 then
-            failwith "What?"
+        let mutable testValue = 0.0
+            
+        for _ in 0 .. maxIterations do
+            let sln = nonMemoizedSolver.Solve network initialState
+            testValue <- sln.FillRates.[sourceTank].Value
+
+        testValue
 
     [<Benchmark>]
     member this.Memoized () =
-        let sln = memoizedSolver.Solve network initialState
-        if sln.FillRates.[source].Value > 1_000_000.0 then
-            failwith "What?"
+        let mutable testValue = 0.0
+
+        for _ in 0 .. maxIterations do
+            let sln = memoizedSolver.Solve network initialState
+            testValue <- sln.FillRates.[sourceTank].Value
+
+        testValue
--- DiscreteRate.Console/Program.fs
+++ DiscreteRate.Console/Program.fs
@@ -5,40 +5,59 @@ open DiscreteRate
 [<EntryPoint>]
 let main argv =
 
-    let source = Tank.create "Source" infinity
-    let op1 = Operation.create "Operation1" 1.0 10.0
-    let op2 = Operation.create "Operation2" 1.0 5.0
-    let merge = Merge.create "Merge1"
-    let split = Split.create "Split1"
-    let tank1 = Tank.create "Tank1" 10.0
-    let sink = Tank.create "Sink" infinity
+
+    //let source = Tank.create "Source" infinity
+    //let op1 = Operation.create "Operation1" 1.0 10.0
+    //let op2 = Operation.create "Operation2" 1.0 5.0
+    //let merge = Merge.create "Merge1"
+    //let split = Split.create "Split1"
+    //let tank1 = Tank.create "Tank1" 10.0
+    //let sink = Tank.create "Sink" infinity
+
+    let operationCount = 10
+
+    let operations =
+        [1 .. operationCount - 1]
+        |> List.map (fun idx -> idx, Operation.create $"Operation{idx}" 1.0 10.0)
+
+    let tanks =
+        [0 .. operationCount - 1]
+        |> List.map (fun idx -> idx, Tank.create $"Tank{idx}" 10.0)
+        |> Map
+
+    let sourceTank = tanks.[0]
+
+    let arcs =
+        operations
+        |> List.collect (fun (idx, operation) -> 
+            let inArc = arc.connect (tanks.[idx - 1], operation)
+            let outArc = arc.connect (operation, tanks.[idx])
+            [inArc; outArc]
+        )
 
     let network =
-        Network [
-            arc.connect (source, split)
-            arc.connect (split, op1, 2.0)
-            arc.connect (split, op2, 1.0)
-            arc.connect (op1, sink)
-            arc.connect (op2, sink)
-        ]
+        Network arcs
 
-    let initialLevels = 
-        [
-            source, TankLevel.Infinite
-            sink, TankLevel.Finite 0.0
-            //tank1, TankLevel.Finite 10.0
-        ] |> Map
+    let tankLevels =
+        tanks
+        |> Map.toSeq
+        |> Seq.map snd
+        |> Seq.map (fun tank -> tank, TankLevel.Finite 0.0)
+        |> Map
+        |> fun x -> x.Add (sourceTank, TankLevel.Infinite)
 
-    let initialState = { TankLevels = initialLevels }
 
-    let settings = {
+    let initialState = { TankLevels = tankLevels }
+
+    let nonMemoizeSettings = {
         MaxSolveTime_ms = 10_000L
         Memoize = false
     }
 
-    let solver = Solver settings
+    let nonMemoizedSolver = Solver nonMemoizeSettings
 
-    let sln = solver.Solve network initialState
+
+    let sln = nonMemoizedSolver.Solve network initialState
 
     //printfn "%A" sln.FlowRates
     //printfn "%A" sln.FillRates
